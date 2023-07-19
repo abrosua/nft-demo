61abcdf5f3cbeb7059e47fcd5891c8d5258aa6c4
abrosua <abrosua@gmail.com>
2023-07-19 12:57:25 +0700

    the generated metadata for samples

---
+++ metadata/sample_metadata.py
@@ -0,0 +1,17 @@
+metadata_template_puppies = {
+    "name": "",
+    "description": "",
+    "image": "",
+    "attributes": [{"trait_type": "cuteness", "value": 100}],
+}
+
+
+metadata_template_puppies_erc1155 = {
+    "name": "",
+    "description": "",
+    "image": "",
+    "attributes": [
+        {"trait_type": "cuteness", "value": 100},
+        {"display_type": "number", "trait_type": "Tier", "value": ""},
+    ],
+}
